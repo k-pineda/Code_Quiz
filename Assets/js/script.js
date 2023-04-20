cf73b5e29eac99413e926bbf54df0273710981af
k-pineda <karenp9310@gmail.com>
2023-04-20 16:01:29 -0400

    updated html & css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,42 +1,22 @@
-var student = document.getElementById("student-names");
-var grade = document.getElementById("grades");
-var comment = document.getElementById("msg");
-var saveButton = document.getElementById("save");
-var savedName = document.getElementById("saved-name");
+var container = document.querySelector(".container");
+var timerEl = document.querySelector("#timer")
 
-function saveLastGrade() {
-  // Save related form data as an object
-  var studentGrade = {
-    student: student.value,
-    grade: grade.value,
-    comment: comment.value.trim()
-  };
-  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-  localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
+// step 2 - timer starts counting down from 75 seconds
+function countdown() {
+  var timeLeft = 75;
+
+  //setInterval(function)
+
+  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    if (timeLeft === 0) {
+      timerEl.textContent = displayMessage();
+      clearsetInterval(timeInterval);
+    }
+
+    timeLeft--
+
+    timerEl.textContent = "Time: " + timeLeft ;
+  }, 75000);
 }
 
-function renderLastGrade() {
-  // Use JSON.parse() to convert text to JavaScript object
-  var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
-  // Check if data is returned, if not exit out of the function
-  if (lastGrade !== null) {
-  document.getElementById("saved-name").innerHTML = lastGrade.student;
-  document.getElementById("saved-grade").innerHTML = lastGrade.grade;
-  document.getElementById("saved-comment").innerHTML = lastGrade.comment;
-  } else {
-    return;
-  }
-}
-
-saveButton.addEventListener("click", function(event) {
-event.preventDefault();
-saveLastGrade();
-renderLastGrade();
-});
-
-// The init() function fires when the page is loaded 
-function init() {
-  // When the init function is executed, the code inside renderLastGrade function will also execute
-  renderLastGrade();
-}
-init();
