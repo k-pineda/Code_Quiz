d96ff09af2db5ec754fd6921682731c134c9b5ec
k-pineda <karenp9310@gmail.com>
2023-04-24 16:13:57 -0400

    added function code to js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,37 +1,48 @@
-var startQuizBtn=document.querySelector("#startbutton")
 var timerEl = document.querySelector("#timer");
-var firstPage=document.querySelector(".firstPage")
-var secondPage=document.querySelector(".secondPage");
+var timeLeft= 0;
+timerEl.textContent = "Time: " + timeLeft ;
 
+var startQuizBtn=document.querySelector("#startbutton");
+var firstPageEl=document.querySelector(".firstPage");
+var secondPageEl=document.querySelector(".secondPage");
 
+var wrong=document.querySelector(".wrong")
+var correct=document.querySelector(".correct")
 
-
+var wrongAnswer1 = document.querySelector(".wrongAnswer1")
+var correctAnswer = document.getElementById("correctAnswer")
 // step 2 - timer starts counting down from 75 seconds
 function countdown() {
   var timeLeft = 75;
-
   //setInterval(function)
   var timeInterval = setInterval(function () {
-    if (timeLeft === 0) {
-      //timerEl.textContent = displayMessage();
-      clearsetInterval(timeInterval);
-    }
-
+    timerEl.textContent = "Time: " + timeLeft ;
     timeLeft--;
 
-    timerEl.textContent = "Time: " + timeLeft ;
-  }, 75000);
+    if (timeLeft === 0) {
+      timerEl.textContent = "";
+      clearsetInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
 }
 
-
-startQuizBtn.addEventListener("click", function (event) {
-// when button is clicked //firstPage.style.display="none"//
-//then show secondPage
-//how to connect the start quiz button to start the timer when clicked
-
-
-//how to connect click of start quiz to the first question promp & answers
-
+function displayWrong () {
+  wrong.classList.remove('hide');
 }
-)
+function displayCorrect () {
+  correct.classList.remove('hide');
+}
+
+function showQuestion1(){
+  firstPageEl.classList.add('hide');
+  secondPageEl.classList.remove('hide')
+}
+
+function startQuiz () {
+  countdown();
+  showQuestion1();
+}
+
+startQuizBtn.addEventListener("click", startQuiz)
 
